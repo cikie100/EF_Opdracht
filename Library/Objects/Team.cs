17999e9e1812944adcc9da5fcadbb79bacf1861a
Crow Scare <ciki3100@gmail.com>
2020-04-28 21:56:03 +0200

    edit

---
+++ Library/Objects/Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Objects
+{
+    public class Team
+    {
+        int stamnummer { get; set; }
+        String naamTrainer { get; set; }
+        List<Speler> spelers { get; set; }
+    }
+}
